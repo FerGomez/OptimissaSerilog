6d929e9e54fbacc2460cb32339fca812959e2b25
RocioV <rocio.velasco@alten.es>
2024-04-24 16:47:52 +0200

    Cambios

---
+++ Prueba1/UnitTest1.cs
@@ -0,0 +1,20 @@
+namespace Prueba1
+{
+    public class Tests
+    {
+
+        private WarningService _logger;
+
+        [SetUp]
+        public void Setup()
+        {
+            _logger = new 
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
